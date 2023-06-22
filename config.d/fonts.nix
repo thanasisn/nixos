15660d72418b6eee466547923e0ea336a40abc30
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-06-22 17:08:22 +0300

    Commit 2023-06-22 17:08

---
+++ config.d/fonts.nix
@@ -0,0 +1,17 @@
+
+## fonts
+
+{ pkgs, ... }:
+{
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+  ];
+}
