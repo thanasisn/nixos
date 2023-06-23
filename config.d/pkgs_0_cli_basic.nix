f280090ffc83b3e69f66dee634376a58b0fa53db
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-06-23 19:09:47 +0300

    Commit 2023-06-23 19:09

--- config.d/pkgs_0_cli_basic.nix
+++ config.d/pkgs_0_cli_basic.nix
@@ -5,30 +5,41 @@
 {
   environment.systemPackages = with pkgs; [
     btrfs-progs
+    curl
     ddrescue
     encfs
     fdupes
     git
+    gnupg
     gocryptfs
+    grep
     htop
     iftop
+    ipset
     lm_sensors
     mc
     ncdu
     nmap
+    openssh-client
     rmlint
     rsync
+    sed
     shellcheck
     smartmontools
+    ssh
     sshfs
+    sudo
+    tee
     testdisk
     tinc
     tmux 
     unison
+    unzip
     vim
     vimPlugins.YouCompleteMe
     vimPlugins.syntastic
     vimPlugins.vim-addon-manager
+    wget
     zfs
     zsh
     zsh-autosuggestions
