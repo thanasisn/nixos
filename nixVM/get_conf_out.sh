133f64b343cd67fb1617c3d5d31bd0dcc52ebc93
NA <NA>
2023-12-08 17:23:08 +0200

    Commit 2023-12-08 17:23

--- nixVM/get_conf_out.sh
+++ nixVM/get_conf_out.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-trach "/home/athan/CODE/nixos"
-scp -r  "athan@192.168.1.104:/home/athan/CODE/nixos" "/home/athan/CODE/"
+# trach "/home/athan/CODE/nixos"
+# scp -r  "athan@192.168.1.104:/home/athan/CODE/nixos" "/home/athan/CODE/"
+rsync -ahrps "athan@192.168.1.104:/home/athan/CODE/nixos"  "/home/athan/CODE"
