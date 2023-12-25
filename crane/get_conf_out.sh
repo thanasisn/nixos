3f3a85b637c42f458d82ba4fe1383de6c49c9b7c
NA <NA>
2023-12-25 20:14:00 +0200

    Commit 2023-12-25 20:14

--- crane/get_conf_out.sh
+++ crane/get_conf_out.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-# trach "/home/athan/CODE/nixos"
-# scp -r  "athan@192.168.1.104:/home/athan/CODE/nixos" "/home/athan/CODE/"
-rsync -av --exclude "*.git*" "athan@155.207.9.214:/home/athan/CODE/nixos" "/home/athan/CODE"
+rsync -ahv --exclude "*.git*" "athan@155.207.9.214:/home/athan/CODE/nixos" "/home/athan/CODE"
